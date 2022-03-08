9b7e6fd2bf10a5b59ca6d097293964f3a552cbf6
Priddle88 <96860899+Priddle88@users.noreply.github.com>
2022-03-08 14:48:35 -0700

    March-08

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-    
-    const textLength = window.prompt("How many characters would you like for your new password?");
 
-}
-generatePassword();
+
+//function to generate the password with a set criteria
+function generatePassword() {
+    //length of password
+    var textLength = window.prompt("How many characters would you like for your new password? (8 - 128)");
+
+    if (textLength < 8 || textLength > 128) { 
+        generatePassword();
+    } else {
+    var lowerCase = window.confirm("Would you like to have lower case letters?");
+    var upperCase = window.confirm("Would you like to have upper case letters?");
+    var numbers = window.confirm("Would you like to add numbers?");
+    var specialChar = window.confirm("Would you like to add special characters?");
+    }
+
+    
+
+    return;
+};
