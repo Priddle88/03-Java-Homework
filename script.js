45cdb39fd27315e779dd7e28e3efdc0d744fc211
Priddle88 <96860899+Priddle88@users.noreply.github.com>
2022-03-09 18:01:30 -0700

    lower/upper case'

--- script.js
+++ script.js
@@ -8,34 +8,51 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  return;
 };
 
 // Add event listener to generate button
+//Starts the code by clicking on the button and activating the WritePassword function)
 generateBtn.addEventListener("click", writePassword);
 
 // Letters for the passcode
 
-    var passLetters = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-    var letter = passLetters.split(" ");
-    var sorted = letter.sort((a, b) => 0.5 - Math.random());
-
+    var passLLetters = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    var passULetters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+    var letterL = passLLetters.split(" ");
+    var letterU = passULetters.split(" ");
+    var sortedL = letterL.sort((a, b) => 0.5 - Math.random());
+    var sortedU = letterU.sort((a, b) => 0.5 - Math.random());
+    var newPassword = [];
+  
 //function to generate the password with a set criteria
 function generatePassword() {
     //length of password
     var textLength = window.prompt("How many characters would you like for your new password? (8 - 128)");
-
     if (textLength < 8 || textLength > 128) generatePassword();
-    
+
     var lowerCase = window.confirm("Would you like to have lower case letters?");
-
-    if (!lowerCase) letter.toUpperCase();
-
     var upperCase = window.confirm("Would you like to have upper case letters?");
     var numbers = window.confirm("Would you like to add numbers?");
     var specialChar = window.confirm("Would you like to add special characters?");
 
-  
-    console.log(letter);
-    return false;
+    
+
+    var conditions = function(){
+      
+      for (i = 0; i < textLength; i++) {
+
+        if (lowerCase) {
+          newPassword.push(sortedL[i]);
+        } else if (upperCase) {
+          newPassword.push(sortedU[i]);
+        }
+    
+      };
+    };
+
+    conditions();
+    console.log(newPassword);
+    return password;
+    return;
 };
